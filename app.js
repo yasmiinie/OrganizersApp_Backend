174043fa179a72442c7eea89a230d868f2dbd299
yasmiinie <ly_zaidi@esi.dz>
2024-05-11 11:26:25 +0100

    database connected

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config(); 
 const express = require('express');
 const app = express();
+const { Sequelize } = require('sequelize');
 const PORT = process.env.PORT || 3000 ;
 
 app.listen(PORT , ()=>{
@@ -8,4 +10,21 @@ app.listen(PORT , ()=>{
 
 app.get('/',(req , res)=>{
     res.send("The backend server is running !")
+});
+
+// connect to the postgres database
+const sequelize = new Sequelize(process.env.DB,{
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // You might need to adjust this based on your PostgreSQL server setup
+        }
+    }
+});
+
+sequelize.sync().then(()=>{
+    console.log('Database connected successfully!');
+}).catch((err)=>{
+    console.error(err);
 });
